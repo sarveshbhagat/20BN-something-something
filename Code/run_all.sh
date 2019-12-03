1abcf658e7d0f0e0741b52ffb1848724f16852d2
pshagnea <57605517+pshagnea@users.noreply.github.com>
2019-12-03 17:29:48 -0500

    Create run_all.sh

---
+++ Code/run_all.sh
@@ -0,0 +1,24 @@
+python3 -W ignore::UserWarning test_models.py somethingv2 \
+                    --weights=pretrained/TSM_somethingv2_RGB_resnet50_shift8_blockres_avg_segment8_e45.pth \
+                                    --test_segments=8 --batch_size=16 -j 12 --test_crops=1 --numpy_file="test_crops-1_resnet50"
+python3 -W ignore::UserWarning test_models.py somethingv2 \
+                    --weights=pretrained/TSM_somethingv2_RGB_resnet50_shift8_blockres_avg_segment8_e45.pth \
+                                    --test_segments=8 --batch_size=16 -j 12 --test_crops=3 --numpy_file="test_crops-3_resnet50"
+python3 -W ignore::UserWarning test_models.py somethingv2 \
+                    --weights=pretrained/TSM_somethingv2_RGB_resnet50_shift8_blockres_avg_segment8_e45.pth \
+                                    --test_segments=8 --batch_size=16 -j 12 --test_crops=5 --numpy_file="test_crops-5_resnet50"
+python3 -W ignore::UserWarning test_models.py somethingv2 \
+                    --weights=pretrained/TSM_somethingv2_RGB_resnet50_shift8_blockres_avg_segment8_e45.pth \
+                                    --test_segments=8 --batch_size=8 -j 12 --test_crops=10 --numpy_file="test_crops-10_resnet50"
+python3 -W ignore::UserWarning test_models.py somethingv2 \
+                    --weights=pretrained/TSM_somethingv2_RGB_resnet50_shift8_blockres_avg_segment8_e45.pth \
+                                    --test_segments=8 --batch_size=16 -j 12 --test_crops=1 --numpy_file="test_crops-1_resnet50"
+python3 -W ignore::UserWarning test_models.py somethingv2 \
+                    --weights=pretrained/TSM_somethingv2_RGB_resnet50_shift8_blockres_avg_segment8_e45.pth \
+                                    --test_segments=8 --batch_size=16 -j 12 --test_crops=3 --numpy_file="test_crops-3_resnet50"
+python3 -W ignore::UserWarning test_models.py somethingv2 \
+                    --weights=pretrained/TSM_somethingv2_RGB_resnet50_shift8_blockres_avg_segment8_e45.pth \
+                                    --test_segments=8 --batch_size=16 -j 12 --test_crops=5 --numpy_file="test_crops-5_resnet50"
+python3 -W ignore::UserWarning test_models.py somethingv2 \
+                    --weights=pretrained/TSM_somethingv2_RGB_resnet50_shift8_blockres_avg_segment8_e45.pth \
+                                    --test_segments=8 --batch_size=8 -j 12 --test_crops=10 --numpy_file="test_crops-10_resnet50"
